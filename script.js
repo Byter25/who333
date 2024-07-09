f557a8519a06a7195f6f7f3bb2086ce5e7d9653f
Byter <bryanchacaliazasaavedra@gmail.com>
2024-07-08 23:47:31 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+const imgs = document.querySelectorAll('.imgRandom img');
+
+// Función para obtener una posición aleatoria
+function getRandomPosition(img) {
+    const x = Math.random() * window.innerWidth - 500;
+    const y = Math.random() * window.innerHeight - 500;
+    return { x, y };
+}
+
+// Función para animar una imagen
+function animateImage(img) {
+    const { x, y } = getRandomPosition(img);
+    img.style.transform = `translate(${x}px, ${y}px)`;
+    img.style.opacity = 1;
+
+    setTimeout(() => {
+        img.style.opacity = 0;
+    }, 1500); // Esconde la imagen después de 1 segundo
+}
+
+// Función para animar todas las imágenes con una posición inicial aleatoria
+function initializeImages() {
+    imgs.forEach(img => {
+        img.addEventListener('load', () => {
+            const { x, y } = getRandomPosition(img);
+            img.style.transform = `translate(${x}px, ${y}px)`;
+            img.style.visibility = "hidden"; // Inicialmente visible para establecer la posición
+        });
+
+        // En caso de que la imagen esté en cache y el evento 'load' no se dispare,
+        // puedes forzar que se cargue aquí también.
+        if (img.complete) {
+            img.dispatchEvent(new Event('load'));
+        }
+    });
+}
+
+
+
+// Mueve las imágenes a nuevas posiciones aleatorias en bucle
+function animateAllImages() {
+    imgs.forEach(img => {
+        setTimeout(() => animateImage(img), Math.random() * 2000);
+        img.style.visibility = "visible"; // Retraso aleatorio para cada imagen
+    });
+}
+
+// Llama a animateAllImages cada 2 segundos
+function comenzar() {
+    // Inicializa las imágenes
+    initializeImages();
+    // Selecciona la imagen con la clase 'kirby'
+    const img = document.querySelector('.principal');
+    if (img) {
+        img.style.visibility = "hidden";
+    }
+
+    const audio = document.getElementById('miAudio');
+    document.body.style.background = 'black'; // Cambia el fondo a negro
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+    audio.volumen = 0.3;
+
+
+
+    setInterval(animateAllImages, 1500);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Selecciona todos los enlaces en la página
+    const links = document.querySelectorAll('a');
+
+    // Agrega target="_blank" a cada enlace
+    links.forEach(link => {
+        link.setAttribute('target', '_blank');
+    });
+    // Agregar el listener para inicializar las imágenes al cargar la página
+    initializeImages();
+});
